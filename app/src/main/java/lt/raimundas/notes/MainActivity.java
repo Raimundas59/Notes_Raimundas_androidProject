8dfc8ffeb494e23db77ccc6c276609ca29868fdb
raimo <raimondas592@gmail.com>
2023-08-29 21:23:56 +0300

    Implement ListView in  MainActivity

--- app/src/main/java/lt/raimundas/notes/MainActivity.java
+++ app/src/main/java/lt/raimundas/notes/MainActivity.java
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import lt.raimundas.notes.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +27,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
-        binding.myTextView.setText("Kokia graži diena ir koks aš nuostabus");
-        binding.myTextView.setTextSize(55);
-        binding.myTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-//        TextView myTextView = findViewById(R.id.myTextView);
-//        myTextView.setText("Kokia graži diena ir koks aš nuostabus");
-//        myTextView.setTextSize(24);
-//        myTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+//        binding.myTextView.setText("Kokia graži diena ir koks aš nuostabus");
+//        binding.myTextView.setTextSize(55);
+//        binding.myTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        ArrayList<String> list = new ArrayList<>();
+       List<String> newlist =  Arrays.asList(
+                "Pirmadienis",
+                "Antradienis",
+                "Trečiadienis",
+                "Ketvirtadienis",
+                "Penktadienis",
+                "Šeštadienis",
+                "Sekmadienis"
+        );
+        list.addAll(newlist);
+        list.addAll(newlist);
+        list.addAll(newlist);
+        list.addAll(newlist);
+        list.addAll(newlist);
+
+        adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                list);
+        binding.notesListView.setAdapter(adapter);
+
     }
 }
